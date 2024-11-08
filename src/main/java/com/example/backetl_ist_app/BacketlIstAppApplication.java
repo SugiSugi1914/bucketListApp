3b6fb4154fe3e06cc2db4a1c897288ae1460663a
tomoki <sugisugi0514@gmail.com>
2024-11-08 09:43:31 +0900

    firstCommit

---
+++ src/main/java/com/example/backetl_ist_app/BacketlIstAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.backetl_ist_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacketlIstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacketlIstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backetl_ist_app/BacketlIstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backetl_ist_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacketlIstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

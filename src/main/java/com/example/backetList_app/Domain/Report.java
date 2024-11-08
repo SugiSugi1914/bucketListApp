b2fbc2280876e58d88b7123cfe0363da685b3236
tomoki <sugisugi0514@gmail.com>
2024-11-08 17:34:15 +0900

    Reportドメインも追加した

---
+++ src/main/java/com/example/backetList_app/Domain/Report.java
@@ -0,0 +1,37 @@
+package com.example.backetList_app.Domain;
+
+public class Report {
+    private Integer id;
+    private String priority;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Report [id=" + id + ", priority=" + priority + "]";
+    }
+
+    public Report() {
+
+    }
+
+    public Report(Integer id, String priority) {
+        this.id = id;
+        this.priority = priority;
+    }
+
+}
---
+++ src/main/java/com/example/backetList_app/Repository/ReportRepository.java
@@ -0,0 +1,40 @@
+package com.example.backetList_app.Repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.backetList_app.Domain.Report;
+
+@Repository
+public class ReportRepository {
+    @Autowired
+    NamedParameterJdbcTemplate template;
+
+    private static final RowMapper<Report> REPORT_ROW_MAPPER = (rs, i) -> {
+        Report report = new Report();
+        report.setId(rs.getInt("id"));
+        report.setPriority(rs.getString("priority"));
+        return report;
+    };
+
+    public List<Report> findAll() {
+        String sql = "SELECT * FROM report ";
+        List<Report> reportList = template.query(sql, REPORT_ROW_MAPPER);
+        return reportList;
+    }
+
+    public Report findById(Integer id) {
+        String sql = "SELECT * FROM report WHERE id= :id";
+        SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+        Report report = template.queryForObject(sql, param, REPORT_ROW_MAPPER);
+        return report;
+
+    }
+
+}
